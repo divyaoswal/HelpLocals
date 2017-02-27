e019d67e8f36cd1fc86ff44bbb7d553558d6b03e
divyaoswal <divya.m.oswal@gmail.com>
2017-02-26 22:32:32 -0800

    changing app.py port

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from project import app
+import os
 # CsrfProtect(app)
 
 
 if __name__ == "__main__":
-	app.run(port=4002, debug=True)
+	port = int(os.environ.get('PORT', 4002))
+	app.run(port=port, debug=False)
